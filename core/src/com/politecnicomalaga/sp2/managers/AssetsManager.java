f960b7d3b2ae9d0abddc74fa8ded55d16b53ba38
Habiee <92110295+Habiee@users.noreply.github.com>
2022-11-09 11:32:50 +0100

    Update AssetsManager.java

--- core/src/com/politecnicomalaga/sp2/managers/AssetsManager.java
+++ core/src/com/politecnicomalaga/sp2/managers/AssetsManager.java
@@ -4,5 +4,10 @@ public class AssetsManager {
     //Constantes
     public static final String NUMBERS_SPRITES = "numbers/digito";
     public static final String NUMBERS_EXT_SPRITES =".png";
+    public static final String SPACESHIP_SPRITES ="player";
+    public static final String ENEMY_SPRITES ="enemy";
+    public static final String ALLY_SHOT ="ally_shot";
+    public static final String ENEMY_SHOT ="enemy_shot";
+    public static final String EXPLOSION_SPRITES ="explosion";
 
 }
