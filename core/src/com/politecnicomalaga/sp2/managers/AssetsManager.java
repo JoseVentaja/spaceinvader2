3d44da52b4a5a8fd1447119c5842ca7fdac04d34
aalg02 <92110199+aalg02@users.noreply.github.com>
2022-11-11 18:22:40 +0100

    Add files via upload

--- core/src/com/politecnicomalaga/sp2/managers/AssetsManager.java
+++ core/src/com/politecnicomalaga/sp2/managers/AssetsManager.java
@@ -1,5 +1,8 @@
 package com.politecnicomalaga.sp2.managers;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
 public class AssetsManager {
     //Constantes
     public static final String NUMBERS_SPRITES = "numbers/digito";
@@ -9,5 +12,15 @@ public class AssetsManager {
     public static final String ALLY_SHOT ="ally_shot";
     public static final String ENEMY_SHOT ="enemy_shot";
     public static final String EXPLOSION_SPRITES ="explosion";
+    public static final String ATLAS_FILE="sp2.atlas";
+    private static Skin textSkin;
+
+
+    public static Skin getTextSkin(){
+        if(textSkin==null){
+            textSkin=new Skin(Gdx.files.internal("glassy-ui.json"));
+        }
+        return textSkin;
+    }
 
 }
