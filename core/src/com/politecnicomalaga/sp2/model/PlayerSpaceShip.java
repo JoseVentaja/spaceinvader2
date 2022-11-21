f92084c52a67ce40bfbc5b7b3a4c6285b3fb1bbb
Leandro808 <92110171+Leandro808@users.noreply.github.com>
2022-11-21 16:38:35 +0100

    Add files via upload

--- core/src/com/politecnicomalaga/sp2/model/PlayerSpaceShip.java
+++ core/src/com/politecnicomalaga/sp2/model/PlayerSpaceShip.java
@@ -6,28 +6,46 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.politecnicomalaga.sp2.managers.AssetsManager;
 import com.politecnicomalaga.sp2.managers.GameManager;
 import com.politecnicomalaga.sp2.managers.SettingsManager;
+import com.politecnicomalaga.sp2.managers.ShootManager;
+
+import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class PlayerSpaceShip extends Actor {
 
     private final Animation<TextureRegion> skin;
-
-    public PlayerSpaceShip() {
+    private Stage baseStage;
+    private ShootManager Shooters;
+    private float shootTime=0;
+    public PlayerSpaceShip(Stage baseStage) {
         super();
+        Shooters = ShootManager.getSingleton();
+        this.baseStage = baseStage;
         TextureAtlas atlas = new TextureAtlas(Gdx.files.internal(AssetsManager.ATLAS_FILE));
-        skin = new Animation<TextureRegion>(SettingsManager.ALLY_SPEED, atlas.findRegions(AssetsManager.SPACESHIP_SPRITES), Animation.PlayMode.LOOP);
-
-        this.setBounds(0, 0, SettingsManager.ALLY_WIDTH, SettingsManager.ALLY_HEIGHT);
-        this.setX(SettingsManager.positionshipX - SettingsManager.ALLY_CENTER);
+        skin = new Animation<TextureRegion>(0.25f, atlas.findRegions(AssetsManager.SPACESHIP_SPRITES), Animation.PlayMode.LOOP);
+        this.setBounds(0, 0,SettingsManager.ALLY_WIDTH,SettingsManager.ALLY_HEIGHT);
+        this.setX(SettingsManager.positionshipX-SettingsManager.ALLY_CENTER);
         this.setY(SettingsManager.positionshipY);
     }
+
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
         TextureRegion currentFrame = skin.getKeyFrame(GameManager.getSingleton().getGameTime(), true);
-        batch.draw(currentFrame, getX(), getY(), SettingsManager.ALLY_WIDTH, SettingsManager.ALLY_HEIGHT);
+        batch.draw(currentFrame, getX(), getY(),SettingsManager.ALLY_WIDTH,SettingsManager.ALLY_HEIGHT);
+        shootTime+=1;
+        if(shootTime>=SettingsManager.tiempoEntreDisparos){
+            shootTime-=SettingsManager.tiempoEntreDisparos;
+            baseStage.addActor(Shooters.shootCreate(getX(),getY()));
+        }
+
+
     }
 }
